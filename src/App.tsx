30d973c0a7612a8f461081170bfcac35ad51197e
Josef Krajkář <josef.krajkar@teta.eu>
2019-08-18 11:24:13 +0200

    create live example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,84 @@
 import * as React from 'react';
-import './App.css';
+import Radial from './Radial';
 
-import logo from './logo.svg';
+import npmIcon from './npm.png';
+import gitIcon from './github.png';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div style={{
+        display: 'flex',
+        flexFlow: 'column'
+      }}>
+        <div style={{
+          flex: '0 0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          fontSize: '36px',
+          fontWeight: 500,
+          marginTop: '50px'
+        }}>
+          Animated gradient DEMO
+        </div>
+        <div style={{
+          flex: '0 0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          fontSize: '26px',
+          fontWeight: 500,
+          marginTop: '20px'
+        }}>
+          Author: Josef Krajkář
+        </div>
+        <div style={{
+          flex: '0 0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          fontSize: '26px',
+          fontWeight: 500,
+          marginTop: '30px'
+        }}>
+          <div style={{
+            flex: '0 0 300px',
+            height: '300px',
+            border: '1px solid #3C3C3C',
+            margin: '10px'
+          }}>
+             <Radial>
+              Click me!
+            </Radial>
+          </div>
+        </div>
+        <div style={{
+          flex: '0 0 auto',
+          display: 'flex',
+          flexFlow: 'row',
+          justifyContent: 'center',
+          marginTop: '80px'
+        }}>
+          <a href='https://www.npmjs.com/package/animated-gradient' target='new'>
+            <img
+              src={npmIcon}
+              style={{
+                width: '80px',
+                height: 'auto',
+                marginRight: '30px'
+              }}
+              alt='NPM'
+            />
+          </a>
+          <a href='https://github.com/josefkrajkar/animated-gradient-demo' target='new'>
+            <img
+              src={gitIcon}
+              style={{
+                width: '80px',
+                height: 'auto'
+              }}
+              alt='GITHUB'
+            />
+          </a>
+        </div>
       </div>
     );
   }
